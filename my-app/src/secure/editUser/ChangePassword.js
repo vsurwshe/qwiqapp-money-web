a6f527a4265f0c0a8d76506810a33c65a083ae6d
dev1 <dev1@124apps.com>
2019-07-19 11:48:58 +0530

    0.14: removed Eye icon

--- my-app/src/secure/editUser/ChangePassword.js
+++ my-app/src/secure/editUser/ChangePassword.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import { Card, CardBody, CardHeader, Alert, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import {Redirect} from 'react-router';
 import { AvField, AvForm } from 'availity-reactstrap-validation';
 import UserApi from '../../services/UserApi';
 import Config from '../../data/Config';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 class ChangePassword extends Component {
     constructor(props) {
@@ -14,8 +12,6 @@ class ChangePassword extends Component {
             passwordUpdated: false,
             color: '',
             content: '',
-            showOldPwdText: false,
-            showPwdText: false
         }
     }
 
@@ -50,16 +46,14 @@ class ChangePassword extends Component {
     }
 
     loadChangePassword = (color, content) => {
-        const {showOldPwdText, showPwdText} = this.state
-        console.log(showOldPwdText)
+        const {oldPassword, newPassword} = this.state
         return (<Card>
             <CardHeader><b>CHANGE PASSWORD</b></CardHeader>
             <CardBody>
                 {color && <Alert color={color}>{content}</Alert>}
                 <AvForm onSubmit={this.updatePassword} >
-                    <AvField name="old" type={showOldPwdText? "text" : "password"} label="Old Password" errorMessage="Enter Correct Password" placeholder="Enter Old Password" value={color === "danger" && ""} required />
-                    <AvField name="new" type={showPwdText ? "text" : "password"} label="New Password" errorMessage="New Password Required" placeholder="Enter  New Password" required />
-                    {showPwdText ? <FaEyeSlash onClick={this.showPwdText} className="float-right" /> :<FaEye onClick={this.showPwdText} className="float-right" />}
+                    <AvField name="old" type={oldPassword? "text" : "password"} label="Old Password" errorMessage="Enter Correct Password" placeholder="Enter Old Password" value={color === "danger" && ""} required />
+                    <AvField name="new" type={newPassword ? "text" : "password"} label="New Password" errorMessage="New Password Required" placeholder="Enter  New Password" required />
                     <center>
                         <Button color="success" >Update</Button>
                         <Link to="/dashboard" style={{ marginLeft: 10 }} ><Button color="secondary" type="button" >Cancel</Button></Link>
@@ -68,24 +62,14 @@ class ChangePassword extends Component {
             </CardBody>
         </Card>);
     }
-    pwdTextOld = () =>{
-        console.log("object")
-        this.setState({ showOldPwdText: !this.state.showOldPwdText });
-    }
-    showPwdText = () => {
-        this.setState({ showPwdText: !this.state.showPwdText });
-    }
 
     loadResponse = (content) => {
         return (<Card>
             <CardHeader><b>CHANGE PASSWORD</b></CardHeader>
-            <CardBody> <center style={{ color: 'green' }}>{content}</center> </CardBody>
-            {this.state.color === "success" && <Redirect to="/dashboard" /> }
-            {this.callTimer()}
+            <CardBody> <center style={{ color: 'green' }}>{content} <br /><br />
+            <Link to="/dashboard" style={{ marginLeft: 10 }} ><Button color="info" type="button" >Dashboard </Button></Link>
+            </center> </CardBody>
         </Card>)
-    }
-    callTimer=()=>{
-        setTimeout(()=>{this.setState({ color: "success" })}, Config.apiTimeoutMillis)
     }
 }
 
--- my-app/src/secure/editUser/EditUser.js
+++ my-app/src/secure/editUser/EditUser.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardBody, CardHeader } from 'reactstrap';
-import {Button} from 'reactstrap';
+import { Button } from 'reactstrap';
 import { AvForm, AvField } from 'availity-reactstrap-validation';
 import { Link } from 'react-router-dom'
 import UserApi from '../../services/UserApi';
@@ -11,28 +11,28 @@ import Store from '../../data/Store';
 class EditUser extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             updated: false,
             user: Store.getUser() ? Store.getUser() : ""
-         }
-    }
-    
-    userUpdate = (event, error, values) =>{
-        if (error.length === 0 ) {
-            new UserApi().updateUser(this.updateSuccessCall, this.updateErrorCall, values);
-        } 
+        }
     }
 
-    updateSuccessCall = (user) =>{
+    userUpdate = (event, error, values) => {
+        if (error.length === 0) {
+            new UserApi().updateUser(this.updateSuccessCall, this.updateErrorCall, values);
+        }
+    }
+
+    updateSuccessCall = (user) => {
         this.setState({ updated: true, content: "User Updated Succesfully !" });
     }
-    updateErrorCall = (error) =>{
-        this.setState({ updated: true,content: "Unable to process request, Please try Again.. " });
+    updateErrorCall = (error) => {
+        this.setState({ updated: true, content: "Unable to process request, Please try Again.. " });
         console.log(error.message)
     }
-    
-    render() { 
-        const {updated, content, user} = this.state;
+
+    render() {
+        const { updated, content, user } = this.state;
         if (updated) {
             return this.loadUpdateSuccess(content);
         } else {
@@ -43,32 +43,32 @@ class EditUser extends Component {
     loadUpdateSuccess = (content) => {
         return (<Card>
             <CardHeader><b>EDIT USER </b></CardHeader>
-            <CardBody><center><b style={{color: "green"}}>{content}</b></center> 
-            {this.callReload()}
+            <CardBody><center><b style={{ color: "green" }}>{content}</b></center>
+                {this.callReload()}
             </CardBody>
         </Card>)
     }
     callReload = () => {
-        setTimeout(()=>{
+        setTimeout(() => {
             window.location.href = "/dashboard";
         }, Config.apiTimeoutMillis);
     }
-    loadEditUser = (user) =>{
-        return ( <Card>
+    loadEditUser = (user) => {
+        return (<Card>
             <CardHeader><b >EDIT USER</b></CardHeader>
             <CardBody>
                 <AvForm onSubmit={this.userUpdate} >
                     <AvField name="email" type="email" label="Email" placeholder="Email" value={user.email} required />
-                    <AvField name="name" type="text" label="User Name" placeholder="User Name" value={user.name} required/>
+                    <AvField name="name" type="text" label="User Name" placeholder="User Name" value={user.name} required />
                     <center>
                         <Button color="success" >Update</Button>
-                        <Link to="/dashboard" style={{marginLeft: 10}} ><Button color="secondary" type="button" >Cancel</Button></Link>
+                        <Link to="/dashboard" style={{ marginLeft: 10 }} ><Button color="secondary" type="button" >Cancel</Button></Link>
                     </center>
                 </AvForm>
             </CardBody>
-            </Card> );
+        </Card>);
     }
 
 }
- 
+
 export default EditUser;
