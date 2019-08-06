38b56555e72ce67af19c5d41164156855f398142
dev1 <dev1@124apps.com>
2019-08-06 16:51:09 +0530

    0.5: react static files.

---
+++ www_bills/precache-manifest.e5366ab70b4b2b3a29af0b015ae24d8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1c72a5a70f88c01702e7255ccf87a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be392641fe90ffeae3ed",
+    "url": "/static/css/2.b0fca259.chunk.css"
+  },
+  {
+    "revision": "be392641fe90ffeae3ed",
+    "url": "/static/js/2.2a4d2cdd.chunk.js"
+  },
+  {
+    "revision": "53ef282ab57feadf50da",
+    "url": "/static/js/3.88c6bd11.chunk.js"
+  },
+  {
+    "revision": "2b7d6968b14425367a0f",
+    "url": "/static/js/main.5b40eb69.chunk.js"
+  },
+  {
+    "revision": "9e375d4c468ca09c9d8f",
+    "url": "/static/js/runtime~main.ab6dc6a4.js"
+  }
+]);
