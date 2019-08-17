404c1413699933f5d2df89d35143724790518504
dev1 <dev1@124apps.com>
2019-08-17 11:58:02 +0530

    0.5: Build the project and copy the content of build to www_bills

---
+++ www_bills/precache-manifest.ab7bb0316ba3d77a4f8550c8597b2d7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "44242775c5a11954cd77",
+    "url": "/static/js/runtime~main.96127c4b.js"
+  },
+  {
+    "revision": "958d74b0242275b1d0b3",
+    "url": "/static/js/main.537c4415.chunk.js"
+  },
+  {
+    "revision": "c001e367848ab69c2f32",
+    "url": "/static/js/3.c860e18b.chunk.js"
+  },
+  {
+    "revision": "26a2855b935dc9aee757",
+    "url": "/static/js/2.c29b6646.chunk.js"
+  },
+  {
+    "revision": "958d74b0242275b1d0b3",
+    "url": "/static/css/main.105b421e.chunk.css"
+  },
+  {
+    "revision": "26a2855b935dc9aee757",
+    "url": "/static/css/2.4eac8efa.chunk.css"
+  },
+  {
+    "revision": "fefc23af56bc81a823072e3ce9f42681",
+    "url": "/index.html"
+  }
+];
