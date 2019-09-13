ecbfbb58bca4ab520d57e2ccee21389f0075b97e
dev1 <dev1@124apps.com>
2019-09-13 12:29:17 +0530

    0.6: dynamic values, links added to dashboard

--- my-app/src/secure/Dashboard.js
+++ my-app/src/secure/Dashboard.js
@@ -1,87 +1,137 @@
 import React, { Component } from "react";
-import { Card, ButtonDropdown, ButtonGroup, CardBody, Row, Col, DropdownItem, DropdownMenu, DropdownToggle } from "reactstrap";
+import { Link } from 'react-router-dom'
+import { Card, CardBody, Row, Col } from "reactstrap";
 import '../css/style.css';
+import BillApi from '../services/BillApi';
+import Store from "../data/Store";
+import Loader from 'react-loader-spinner';
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      bills: [],
+      recurBills: [],
+      paidBills: 0,
+      unpaidBills: 0,
+      overdueBills: 0,
+      upcomingBills: 0,
+      spinner: true
+    };
   }
 
-  loading = () => (
-    <div className="animated fadeIn pt-1 text-center">Loading...</div>
-  );
-
-  toogleUpcomingBillCard = ()=>{
-    this.setState({ upcomingBill: !this.state.upcomingBill }); 
+  componentDidMount = () => {
+    this.setProfileId();
   }
 
-  toogleOverDueBillCard = ()=>{
-    this.setState({ overDueBill: !this.state.overDueBill }); 
+  // Setting ProfileId from Store
+  setProfileId = async () => {
+    if (Store.getProfile()) {
+      await this.setState({ profileId: Store.getProfile().id });
+      await this.getAllBills();
+    }
   }
 
-  tooglePaidBillCard = ()=>{
-    this.setState({ paidBill: !this.state.paidBill }); 
+  // Getting Bills for specific profile
+  getAllBills = async () => {
+    await new BillApi().getBills(this.billsSuccessCall, this.errorCall, this.state.profileId)
   }
 
-  toogleUnpaidBillCard = ()=>{
-    this.setState({ unPaidBill: !this.state.unPaidBill }); 
+  // Getting Bills SuccessCall
+  billsSuccessCall = async (bills) => {
+    if (bills.length === 0) {
+      this.setState({ bills: [0] });
+    } else {
+      this.setState({ bills });
+      this.calcPaidUnpaidBills(bills)
+    }
+    this.setState({ spinner: false })
+  }
+
+  errorCall = (error) => {
+    console.log("Error", error)
+  }
+
+  //Calculate paid and unpaid bills count and set to state
+  calcPaidUnpaidBills = (bills) => {
+    let paidBills = 0, unpaidBills = 0;
+    bills.map(bill => bill.paid ? paidBills++ : unpaidBills++)
+    this.setState({ paidBills, unpaidBills });
+    this.calcOverDueUpcomingBills(bills)
+  }
+
+  // Calculate Overdue, Upcoming bills for bills, recurring bills
+  calcOverDueUpcomingBills = (bills) => {
+    let overdueBills = 0, upcomingBills = 0;
+    bills.map(bill => this.loadDateFormat(bill.dueDate_) >= new Date() ? upcomingBills++ : overdueBills++)
+    this.setState({ overdueBills: this.state.overdueBills + overdueBills, upcomingBills: this.state.upcomingBills + upcomingBills });
   }
 
   render() {
-    return <div>{this.loadDashboard()}</div>
+    //shows spinner until getting values from api
+    if (this.state.spinner) {
+      return <div>{this.loadSpinner()}</div>
+    } else {
+      return <div>{this.loadDashboard()}</div>
+    }
+  }
+
+  //Shows spinner  
+  loadSpinner = () => {
+    return <center style={{ paddingTop: '20px' }}>
+      <Loader type="TailSpin" color="#2E86C1" height={60} width={60} />
+    </center>
+  }
+
+  //converts the duedate to regular date format
+  loadDateFormat = (dateParam) => {
+    let toStr = "" + dateParam
+    let dateString = toStr.substring(0, 4) + "-" + toStr.substring(4, 6) + "-" + toStr.substring(6, 8)
+    let date = new Date(dateString);
+    return date;
   }
 
   // hard coded added for now...
   loadDashboard = () => {
     return <div className="animated fadeIn">
       <Row>
-        {/* Upcomeing Bills */}
+        {/* Upcoming Bills */}
         <Col xs="12" sm="6" lg="3">
           <Card className="text-white bg-warning">
-            {this.loadBillDivision(this.state.upcomingBill, this.toogleUpcomingBillCard, 12, "Upcoming Bills")}
+            {this.loadBillDivision(this.state.upcomingBills, "Upcoming Bills", '/listBills')}
           </Card>
         </Col>
         {/* Overdue Bills */}
         <Col xs="12" sm="6" lg="3">
           <Card className="text-white bg-danger">
-            {this.loadBillDivision(this.state.overDueBill, this.toogleOverDueBillCard, 21, "Over Due")}
+            {this.loadBillDivision(this.state.overdueBills, "OverDue Bills", '/listBills')}
           </Card>
         </Col>
         {/* Paid Bills */}
         <Col xs="12" sm="6" lg="3">
           <Card className="text-white bg-success">
-            {this.loadBillDivision(this.state.paidBill, this.tooglePaidBillCard, 25, "Paid Bills")}
+            {this.loadBillDivision(this.state.paidBills, "Paid Bills", '/listBills')}
           </Card>
         </Col>
         {/* Unpaid Bills */}
         <Col xs="12" sm="6" lg="3">
           <Card className="text-white bg-info">
-            {this.loadBillDivision(this.state.unPaidBill, this.toogleUnpaidBillCard, 11, "Unpaid Bills")}
+            {this.loadBillDivision(this.state.unpaidBills, "Unpaid Bills", '/listBills')}
           </Card>
         </Col>
       </Row>
     </div>
   }
 
-  loadBillDivision = (isOpen, handleCard, value, billType) => {
+  loadBillDivision = (value, billType, url) => {
     return <>
       <CardBody className="pb-0">
-        <div>{billType}</div>
-        <ButtonGroup className="float-right">
-            <ButtonDropdown id='upcomingBill' isOpen={isOpen} toggle={() => {handleCard()}}>
-              <DropdownToggle caret className="p-0" color="transparent">
-                <i className="icon-settings"></i>
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Action</DropdownItem>
-                <DropdownItem>Another action</DropdownItem>
-              </DropdownMenu>
-            </ButtonDropdown>
-          </ButtonGroup>
+        <div className="text-value">{billType}</div>
         <div className="text-value"> {value} </div>
       </CardBody>
-      <div className="chart-wrapper mx-3" style={{ height: '70px' }}></div>
+      <div className="chart-wrapper mx-3" style={{ height: 40, marginTop: 40 }}>
+        <div ><Link to={url} style={{ color: '#ffffff' }}>View {billType}</Link></div>
+      </div>
     </>
   }
 }
