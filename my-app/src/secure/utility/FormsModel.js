275685bc9cc8e733376ff8051434df1609f80353
DeveloperTwo <dev2@124apps.com>
2019-11-26 14:21:21 +0530

    0.8: rebased with master

--- my-app/src/secure/utility/FormsModel.js
+++ my-app/src/secure/utility/FormsModel.js
@@ -90,7 +90,7 @@ export const CategoryLabelForm = (props) => {
         <Label><>{componentType} Name <b className="text-color"> * </b></></Label>
       </Col>
       <Col sm={6}>
-        <AvField type="text" name="name" errorMessage="Category Name Required" value={itemName} placeholder="Enter Category name" required />
+        <AvField type="text" name="name" errorMessage={componentType + " name required"} value={itemName} placeholder={"Enter " + componentType.toLowerCase() +  " name"} required />
       </Col>
     </Row>
     {componentType === "Label" ?
@@ -98,13 +98,10 @@ export const CategoryLabelForm = (props) => {
         <Col sm={3} >
           <Label>Description / Notes </Label>
         </Col>
-        <Col sm={6}>
-          <AvField type="text" name="notes" value={notes} placeholder="Description / Notes" />
-        </Col>
+        <Col sm={6}> <AvField type="text" name="notes" value={notes} placeholder="Description / Notes" /> </Col>
       </Row>
       : <Row>
-        <Col sm={3} >
-          <Label>Type</Label></Col>
+        <Col sm={3} > <Label>Type</Label> </Col>
         <Col sm={6}><AvField type="select" name="type" value={type ? type : billType.PAYABLE} errorMessage="Select Type of Category" >
           <option value={billType.PAYABLE}>Payable</option>
           <option value={billType.RECEIVABLE}>Receivable</option>
@@ -112,35 +109,29 @@ export const CategoryLabelForm = (props) => {
       </Row>
     }
     <Row>
-      <Col sm={3} >
-        <Label>{componentType + " color"} </Label>
-      </Col>
-      <Col sm={6}><AvField type="color" name="color" list="colors" value={itemColor} />
-      </Col>
+      <Col sm={3} > <Label>{componentType + " color"} </Label> </Col>
+      <Col sm={6}><AvField type="color" name="color" list="colors" value={itemColor} /> </Col>
     </Row>
 
     {items && (items.length > 0 && // checking Label / Categories are there, then only showing "Nest option" while creating Label / Categories 
       (updateItem ? // checking the item(Label / Categories) is creating / updating, if creating then showing "Nest option"
-        (updateItem.parentId ? // Checking whether Label / Categories has ParentId. If parentId is there then we are showing "Make it as Parent" or else checking for subLabel/subcategory 
+        (updateItem.parentId ? // Checking whether Label / Categories has parentId. If parentId is there then we are showing "Make it as Parent" or else checking for subLabel/subcategory 
           (!chkMakeParent && <><Label style={{ paddingLeft: 20 }} check>
             <AvInput type="checkbox" name="makeParent" onChange={props.toggle} /> Make it as Parent </Label> <br /></>) // if selected make it as parent, then assigning "null" to "parentId"
-          : !(updateItem.subLabels || updateItem.subCategories) &&
+          : !(updateItem.subLabels || updateItem.subCategories) && // Checking whether Label / Categories has subLabels/subCategories. If subLabels/subCategories are not there then showing "nest option"
           <><Label style={{ paddingLeft: 20 }} check>
-            <AvInput type="checkbox" name="checkbox1" onChange={props.toggle} /> Nest {componentType} under </Label> <br /><br /> </>) //checking for subItems, if there dont show anything or else showing "Nest option"
+            <AvInput type="checkbox" name="checkbox1" onChange={props.toggle} /> Nest {componentType.toLowerCase()} under </Label> <br /><br /> </>) //checking for subItems, if there dont show anything or else showing "Nest option"
         : <><Label style={{ paddingLeft: 20 }} check>
-          <AvInput type="checkbox" name="checkbox1" onChange={props.toggle} /> Nest {componentType} under </Label> <br /> <br /></>) // If creating Label/ category then showing "Nest option"
+          <AvInput type="checkbox" name="checkbox1" onChange={props.toggle} /> Nest {componentType.toLowerCase()} under </Label> <br /> <br /></>) // If creating Label/ category then showing "Nest option"
       )
     }
-    
 
     <Collapse isOpen={collapse}>
       <Row>
-        <Col sm={3}>
-          <Label>{"Select parent " + componentType.toLowerCase()}</Label>
-        </Col>
+        <Col sm={3}> <Label>{"Select parent " + componentType.toLowerCase()}</Label> </Col>
         <Col sm={6}>
           <AvField type="select" name="parentId" value={parentId} required={collapse}>
-            <option value="">Select {componentType}</option>
+            <option value="">Select parent {componentType}</option>
             {items.map((item, key) => { return <option key={key} value={item.id}>{item.name}</option> })}
           </AvField>
         </Col>
