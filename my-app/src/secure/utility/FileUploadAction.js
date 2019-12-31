2bafe79c85b79cf73bfcb8caf9eb264567a5c19d
DeveloperTwo <dev2@124apps.com>
2019-12-31 13:02:03 +0530

    0.8: deleted unwanted files

--- my-app/src/secure/utility/FileUploadAction.js
+++ my-app/src/secure/utility/FileUploadAction.js
@@ -1,14 +1,12 @@
 import React, { Fragment } from 'react';
-import { Row, Button } from 'reactstrap';
+import { Row } from 'reactstrap';
 import { FaFile } from "react-icons/fa";
 import Files from 'react-files';
 
 export const FileUploadForm = (props) => < Fragment>
-  <span> supported files (<b>Ex: </b> image/*, audio/*, video/mp4, text, .pdf, .xlsx, .docx, .doc) </span>
-  <Files accepts={['image/*', 'audio/*', 'video/mp4', 'text/*', '.pdf', '.xlsx', '.docx', '.doc']}
-    onChange={props.handleInput} multiple={false} clickable >
-    <Button color="link"><u>click here</u></Button> to upload a file
-  </Files>
+  <p> supported files (<b>Ex: </b> image/*, audio/*, video/mp4, text, .pdf, .xlsx, .docx, .doc) </p>
+  <Files onChange={props.handleInput} accepts={['image/*', 'audio/*', 'video/mp4', 'text/*', '.pdf', '.xlsx', '.docx', '.doc']} 
+    multiple={false} clickable > <u>click here</u> to upload a file </Files>
 </Fragment>
 
 export const FilePreview = (props) => {
