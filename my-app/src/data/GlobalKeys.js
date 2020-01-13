7a13a679fbe567a829b087b2effcec805a36423d
DeveloperTwo <dev2@124apps.com>
2020-01-13 16:04:50 +0530

    0.8: removed string litrals for Contact API

--- my-app/src/data/GlobalKeys.js
+++ my-app/src/data/GlobalKeys.js
@@ -73,3 +73,10 @@ export const moreOptions = {
    ATTACHMENTS: 'Attachments',
    DELETE: 'Delete'
  }
+
+ export const apiMethod = {
+    GET: "GET",
+    POST: "POST",
+    PUT: "PUT",
+    DELETE: "DELETE"
+ }
